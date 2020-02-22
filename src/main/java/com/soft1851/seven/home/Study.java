4ca20ccffc5e7bb48b296b6d84aac5954377ab46
1299088269 <1299088269@qq.com>
2020-02-22 23:03:29 +0800

    Initial commit

---
+++ src/main/java/com/soft1851/seven/home/Study.java
@@ -0,0 +1,13 @@
+package com.soft1851.seven.home;
+
+/**
+ * @ClassName Study
+ * @Description TODO
+ * @Author 田震
+ * @Date 2020/2/22
+ **/
+public class Study {
+    public static void main(String[] args) {
+        System.out.println("我是第七组");
+    }
+}
