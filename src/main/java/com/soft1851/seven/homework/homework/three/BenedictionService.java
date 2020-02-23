2e9b4321390dcd417856e013dfbd8729a308f472
qixiao <zhengliang008@qq.com>
2020-02-23 14:20:01 +0800

    郑亮

---
+++ src/main/java/com/soft1851/seven/homework/homework/three/BenedictionService.java
@@ -0,0 +1,11 @@
+package com.soft1851.seven.homework.homework.three;
+
+/**
+ * @author ：zhengliang
+ * @date ：2020/2/23 13:57
+ * @description：TODO
+ */
+@FunctionalInterface
+public interface BenedictionService {
+    void  sayBendiction(String name,String bendiction);
+}
